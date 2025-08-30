c94e50be217d36e40a7013b5425551430e89638f
ashik081 <freelancerashik81@gmail.com>
2025-08-30 20:21:02 +0600

    add

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const plusBtn = document.getElementById("increase-btn");
+const minusBtn = document.getElementById("decrease-btn");
+const resetBtn = document.getElementById("rest-btn");
+const counter = document.getElementById("counter");
+const max = 10;
+const min = 0;
+//normal function
+// function updateValue(value) {
+//   counter.value = Number(counter.value) + value;
+// }
+
+// arrow function
+const updateValue = (value) => {
+  counter.value = Number(counter.value) + value;
+
+  plusBtn.disabled = Number(counter.value) >= max ? true : false;
+  minusBtn.disabled = Number(counter.value) <= min ? true : false;
+};
+
+plusBtn.addEventListener("click", () => {
+  updateValue(1);
+});
+
+minusBtn.addEventListener("click", () => {
+  updateValue(-1);
+});
+
+resetBtn.addEventListener("click", () => {
+  counter.value = "0";
+  updateValue(0);
+});
+
+counter.value = "0";
+plusBtn.disabled = false;
+minusBtn.disabled = true;
+// function updateValue(value) {
+//   counter.value = Number(counter.value) + value;
+
+//   plusBtn.disabled = Number(counter.value) >= max ? true : false;
+//   minusBtn.disabled = Number(counter.value) <= min ? true : false;
+// }
+
+// plusBtn.addEventListener("click", function () {
+//   updateValue(+1);
+// });
+
+// minusBtn.addEventListener("click", function () {
+//   updateValue(-1);
+// });
